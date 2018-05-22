45a712bfbc8608cfeb07c5bffe54e076f149b06d
Michael Irby <michael.irbstoomuch@gmail.com>
2018-05-21 20:47:04 -0400

    updating repo

--- rn-course/App.js
+++ rn-course/App.js
@@ -5,7 +5,7 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Test</Text>
+        <Text>Something Changed!</Text>
       </View>
     );
   }
